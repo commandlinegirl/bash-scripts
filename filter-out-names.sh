1c19a9ff8342a62b59f9e9510f7cd91036b3b16a
commandlinegirl <commandlinegirl@users.noreply.github.com>
2016-12-13 19:53:21 -0800

    Added a few snippets

--- filter-out-names.sh
+++ filter-out-names.sh
@@ -1,12 +1,19 @@
 #!/usr/env/bin bash
 
-# Read country names into an array and then filter out all 
-# the names containing the letter 'a' or 'A'.
-
+# Read country names into an array 
 countries=('')
 while read country; do
    countries=("${countries[@]}" $country)
 done
 
+# Filter out all the names containing the letter 'a' or 'A'.
 echo ${countries[@]/*[Aa]*/}
 
+# Replace capital letters with a dot
+echo ${countries[@]/[:A-Z:]/\.}
+
+# Length of the element located at index 0
+echo ${#countries[0]}
+
+# Slice the array (display the elements between position 3 and 5, both inclusive
+echo ${countries[@]:3:5}
